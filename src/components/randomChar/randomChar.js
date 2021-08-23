31ca2c0e507f437a21028d4e62e64c1cce5801f6
kozlov-oe <O.Kozlov@otpbank.ru>
2021-08-23 09:57:12 +0300

    fixes styles in project && added got service

--- src/components/randomChar/randomChar.js
+++ src/components/randomChar/randomChar.js
@@ -1,12 +1,24 @@
 import React, {Component} from 'react';
-import './randomChar.css';
-
+import styled from "styled-components";
 export default class RandomChar extends Component {
 
     render() {
+        const RandomBlock = styled.div`
+          background-color: #fff;
+          padding: 25px 25px 15px 25px;
+          margin-bottom: 40px;
+          
+          h4 {
+            margin-bottom: 20px;
+            text-align: center;
+          }
 
+          .term {
+            font-weight: bold;
+          }
+        `;
         return (
-            <div className="random-block rounded">
+            <RandomBlock className="rounded">
                 <h4>Random Character: John</h4>
                 <ul className="list-group list-group-flush">
                     <li className="list-group-item d-flex justify-content-between">
@@ -26,7 +38,7 @@ export default class RandomChar extends Component {
                         <span>Anarchy</span>
                     </li>
                 </ul>
-            </div>
+            </RandomBlock>
         );
     }
 }
